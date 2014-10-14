504319e51a5a64c85e8bdbbeb51243aebfd7e68f
andrewio <andrewio@ya.ru>
2014-10-14 23:00:19 +0500

    DrawPoint

---
+++ 2DAnimationEditor/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace _2DAnimationEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SceneView = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.SceneView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SceneView
+            // 
+            this.SceneView.Location = new System.Drawing.Point(12, 12);
+            this.SceneView.Name = "SceneView";
+            this.SceneView.Size = new System.Drawing.Size(727, 254);
+            this.SceneView.TabIndex = 0;
+            this.SceneView.TabStop = false;
+            this.SceneView.Paint += new System.Windows.Forms.PaintEventHandler(this.SceneView_Paint);
+            this.SceneView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SceneView_MouseDown);
+            this.SceneView.MouseMove += new System.Windows.Forms.MouseEventHandler(this.SceneView_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 296);
+            this.Controls.Add(this.SceneView);
+            this.Name = "Form1";
+            this.Text = "2D Animation Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SceneView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox SceneView;
+    }
+}
+
---
+++ 2DAnimationEditor/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace _2DAnimationEditor
+{
+    public partial class Form1 : Form
+    {
+
+
+        // Each polygon is represented by a List.
+        private List<Point> Vertices = new List<Point>();
+
+        // The current mouse position while drawing a new polygon.
+        private Point NewPoint;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        // Start or continue drawing a new polygon.
+        private void SceneView_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Vertices.Add(e.Location);
+
+            // Redraw.
+            SceneView.Invalidate();
+        }
+
+
+      
+
+        // Move the next point in the new polygon.
+        private void SceneView_MouseMove(object sender, MouseEventArgs e)
+        {
+            NewPoint = e.Location;
+            SceneView.Invalidate();
+        }
+
+        // Redraw old polygons in blue. Draw the new polygon in green.
+        // Draw the final segment dashed.
+        private void SceneView_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.Clear(SceneView.BackColor);
+
+            foreach (var vertex in this.Vertices)
+            {
+                DrawVertex(vertex, e);
+            }
+
+            DrawVertex(NewPoint,e);
+        }
+
+
+        private void DrawVertex(Point p, PaintEventArgs e)
+        {
+            int vertexWidth = 40;
+            Rectangle rect = new Rectangle(p.X - vertexWidth / 2, p.Y - vertexWidth / 2, vertexWidth, vertexWidth);
+            e.Graphics.DrawEllipse(Pens.Aqua, rect);
+            e.Graphics.FillEllipse(Brushes.Black, rect);
+        }
+    }
+}
